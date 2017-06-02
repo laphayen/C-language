f67fd5291f6711166ad3fc429c3eba4772c14054
Baek Gi Chan <11618nathan@gmail.com>
2017-06-02 20:12:15 +0900

    basic form

--- Hello.c
+++ Hello.c
@@ -1,8 +1,13 @@
-#include <stdio.h>
+#include <stdio.h>		//헤더 파일 선언
 
+// main 함수 시작
 int main(void)
 {
-	// 출력 함수
-	printf("Hello world!\n");
-	return 0;
+	/*
+	블록 주석
+	*/
+	// 행 단위 주석
+	printf("==Hello world==");	// 문자열 출력 함수
+	return 0; // 0반환
 }
+// 함수 끝
