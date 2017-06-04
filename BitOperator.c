91a6a76c9bef7232eae44f79712c44c8305e9c3f
Baek Gi Chan <11618nathan@gmail.com>
2017-06-04 21:57:48 +0900

    Update BitOperator.c

--- BitOperator.c
+++ BitOperator.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 void main()
@@ -6,27 +7,27 @@ void main()
 	int num2 = 15;	// 00000000 00000000 00000000 00001111
 	int result1, result2, result3, result4, result5, result6;
 
-	// &¿¬»êÀÚ: AND - 1&1->1
+	// &ì—°ì‚°ìž: AND - 1&1->1
 	result1 = num1 & num2; 
-	printf("result1: %d \n", result1); 
+	printf("& AND result1: %d \n", result1); 
 	
-	// |¿¬»êÀÚ: OR - 1|1, 1|0, 0|1 -> 1
+	// |ì—°ì‚°ìž: OR - 1|1, 1|0, 0|1 -> 1
 	result2 = num1 | num2;
-	printf("result2: %d \n", result2);
+	printf("| OR result2: %d \n", result2);
 
-	// ^¿¬»êÀÚ: XOR - 0^1, 1^0 -> 1
+	// ^ì—°ì‚°ìž: XOR - 0^1, 1^0 -> 1
 	result3 = num1 ^ num2;
-	printf("result3: %d \n", result3);
+	printf("^ XOR result3: %d \n", result3);
 
-	// ~¿¬»êÀÚ: NOT
+	// ~ì—°ì‚°ìž: NOT - 0->1, 1->0 ë³´ìˆ˜ ì—°ì‚°
 	result4 = ~num1;
-	printf("result4: %d \n", result4);
+	printf("~ NOT result4: %d \n", result4);
 
-	// <<¿¬»êÀÚ: ¿ÞÂÊ ÀÌµ¿
+	// <<ì—°ì‚°ìž: ì™¼ìª½ ì´ë™
 	result5 = num1 << 1;
-	printf("result5: %d \n", result5);
+	printf("<< result5: %d \n", result5);
 
-	// >>¿¬»êÀÚ: ¿À¸¥ÂÊ ÀÌµ¿
-	result6 = num2 >> 2;
-	printf("result6: %d \n", result6);
+	// >>ì—°ì‚°ìž: ì˜¤ë¥¸ìª½ ì´ë™
+	result6 = num2 >> 2; 
+	printf(">> result6: %d \n", result6);
 }
