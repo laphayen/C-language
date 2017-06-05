3d0b7a72ce02a185bc4431eb5e041e3c3e3a9b75
Baek Gi Chan <11618nathan@gmail.com>
2017-06-05 20:24:49 +0900

    Update if-Basic.c

--- if-Basic.c
+++ if-Basic.c
@@ -5,10 +5,15 @@ void main()
 {
 	int num = 11618;
 
+	// if - 식
 	if (num == 11618)
 		printf("num은 11618입니다.");
+	
+	// if가 아닐 경우 실행
 	else if (num > 11618)
 		printf("num은 11618보다 큽니다.");
+	
+	// if와 else가 아닐 경우 실행
 	else
 		printf("num은 11618이 아닙니다.");
 }
