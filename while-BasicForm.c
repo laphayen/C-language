059660d33b42115b94d7e26a6b732db9ea21deec
Baek Gi Chan <11618nathan@gmail.com>
2017-06-06 16:22:48 +0900

    Update while-BasicForm.c

--- while-BasicForm.c
+++ while-BasicForm.c
@@ -4,8 +4,8 @@
 void main()
 {
 	int num = 0;
-	int i = 2;
-	int j = 0;
+	int dan = 2;
+	int num = 0;
 
 	while (num < 10)	// 반복의 조건 - 10회 반복
 	{
@@ -14,30 +14,31 @@ void main()
 		// 무한 루프 방지 -> 무한 루프 - Ctrl+C
 		num++;
 	}
-	
+
+
 	// 중괄호 생략 가능 - 1줄 실행
 	while (num < 10)
 		printf("while문 반복 %d \n", num, num++);
-		
-	
+
+
 	// 무한 루프
 	while (1)
 	{
 		printf("무한 루프 반복 \n");
 	}
 
-	
-	// while 중첩
+
+	// while 중첩 - 전체 출력
 	// Ex) multiplicationTable
-	while (i<10)
+	while (dan<10)
 	{
-		j = 1;
-		while (j<10)
+		num = 1;
+		while (num <10)
 		{
-			printf("%d * %d = %d \n", i, j, i*j);
-			j++;
+			printf("%d * %d = %d \n", dan, num, dan*num);
+			num++;
 		}
-		i++;
+		dan++;
 	}
 
 	return 0;
