084ce5dd000e34cbc1b0b2e39189b1cd57487db5
Baek Gi Chan <11618nathan@gmail.com>
2017-06-06 16:20:37 +0900

    Update multiplicationTable.c

--- multiplicationTable.c
+++ multiplicationTable.c
@@ -7,18 +7,18 @@ int main(void)
 	int num = 1;
 
 	printf("몇 단? \n");
+
 	// 단 입력
 	scanf("%d", &dan);
 
-	// while 1부터 9까지 곱
+	// while - 1부터 9까지 곱
 	while (num < 10)
 	{
 		printf("%d * %d = %d \n", dan, num, dan*num);
 		num++;
 	}
 	
-		// while 중첩
-	// Ex) multiplicationTable
+	// while 중첩 - 전체 출력
 	while (dan<10)
 	{
 		num = 1;
@@ -29,5 +29,6 @@ int main(void)
 		}
 		dan++;
 	}
+
 	return 0;
 }
