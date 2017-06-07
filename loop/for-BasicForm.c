492f79c1636040e1f23f16a0364ea4d8494e07ca
Baek Gi Chan <11618nathan@gmail.com>
2017-06-07 11:39:16 +0900

    Add files via upload

---
+++ loop/for-BasicForm.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+void main()
+{
+	// 초기식 - 1번 실행
+	int num = 0;
+	int sum = 0;
+	int i;
+	int dan = 0;
+
+	for ( num = 0; num < 3; num++)	// 조건식 - 반복 결정, 증감식 - 마지막 연산
+	{
+		printf("num: %d \n", num);
+	}
+	puts("");
+
+	return 0;
+}
