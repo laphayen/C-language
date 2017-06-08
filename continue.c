9afe8fccf2f14b23d48ee8ccb0103fa05206ab52
Baek Gi Chan <11618nathan@gmail.com>
2017-06-08 19:53:26 +0900

    Add files via upload

---
+++ continue.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+void main()
+{
+	int num = 1;
+	printf("start! \n");
+
+	while (num < 101)
+	{
+		if(num % 2 == 0)
+		{
+			continue;
+		}
+		printf("%d \n", num);
+		num++;
+	}
+	printf("end! \n");
+
+	return 0;
+}
