ae45f68288d24452429f8952a48ba38b307de185
Baek Gi Chan <11618nathan@gmail.com>
2017-06-09 15:42:33 +0900

    Add files via upload

---
+++ switch-BasicForm.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+void main()
+{
+	int num;
+	printf("1~3 정수 입력: ");
+	scanf("%d", &num);
+
+	switch (num)
+	{
+	case 1:
+		printf("1 입력 \n");
+		break;
+	case 2:
+		printf("2 입력 \n");
+		break;
+	case 3:
+		printf("3 입력 \n");
+		break;
+	default:
+		break;
+	}
+}
