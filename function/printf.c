0fc45e75d4dc478071edc7add84ac0bf66ca9247
Baek Gi Chan <11618nathan@gmail.com>
2017-06-10 21:12:33 +0900

    Update printf.c

--- function/printf.c
+++ function/printf.c
@@ -5,7 +5,12 @@ void main()
 {
 	int num1;
 	int num2;
+	int num3;
+
 	num1 = printf("11618\n");
 	num2 = printf("nathan\n");
+	num3 = printf("GitHub\n");
+
+	// 문자열 길이 반환 - \n 포함
 	printf("%d %d \n", num1, num2);
 }
