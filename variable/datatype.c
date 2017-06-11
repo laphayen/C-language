5bf48c73154eef7bfa5f73d62b987341058ec888
Baek Gi Chan <11618nathan@gmail.com>
2017-06-11 17:17:45 +0900

    Create datatype.c

---
+++ variable/datatype.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	// 정수형
+	char ch = 'a';				// 1바이트
+	short sh = 11618;			// 2바이트
+	int num = 11618;			// 4바이트
+	long lo = 11618;			// 4바이트
+	long long lolo = 11618;		// 8바이트
+								
+	// 실수형
+	float fl = 1.1618;			// 4바이트
+	double db = 1.1618;			// 8바이트
+	long double lod = 1.1618;	// 8바이트
+}
