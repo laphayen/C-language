ce6772390406aa6cdbd55b9d0bd6dc10cd9e5a8a
Baek Gi Chan <11618nathan@gmail.com>
2017-06-17 00:27:58 +0900

    Rename strcpy.c to function/strcpy.c

---
+++ function/strcpy.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+// 문자열 함수 헤더 파일 선언
+#include <string.h>
+
+void main()
+{
+	// strcpy - 문자열 복사 함수
+
+	char original[] = "11618nathan";
+
+	// 복사할 파일보다 배열의 크기 이상 지정 -> 오버플로우 방지
+	char copy[20];
+
+	// (복사할 위치, 위치);
+	strcpy(copy, original);
+
+	printf("%s \n", copy);
+}
