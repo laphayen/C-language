3e6c056026046ff6d41160775c1e827d7270b08a
Baek Gi Chan <11618nathan@gmail.com>
2017-06-23 21:55:30 +0900

    Update CharacterString.c

--- array/CharacterString.c
+++ array/CharacterString.c
@@ -12,5 +12,12 @@ void main()
 	// 배열 &필요X
 	scanf("%s", name);	
 
+	// null문자 삽입(\0) - 특수문자(escape sequence) 자동 삽입
 	printf("이  름: %s \n", name);
+
+	char name[32] = "11618nathan";
+
+	// 배열 크기
+	printf("배열의 크기: \d", sizeof(name));
+
 }
