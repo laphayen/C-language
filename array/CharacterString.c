53c100a54546dbafc07e4539d547547f78ba00d7
Baek Gi Chan <11618nathan@gmail.com>
2017-06-23 21:59:38 +0900

    Update CharacterString.c

--- array/CharacterString.c
+++ array/CharacterString.c
@@ -6,18 +6,18 @@ void main()
 	// char배열을 이용해 문자열 입력
 	int i;
 	char name[32];
+	char id[] = "11618nathan";
 
 	printf("이  름: ");
-	
+
 	// 배열 &필요X
-	scanf("%s", name);	
+	scanf("%s", name);
 
 	// null문자 삽입(\0) - 특수문자(escape sequence) 자동 삽입
 	printf("이  름: %s \n", name);
 
-	char name[32] = "11618nathan";
-
 	// 배열 크기
-	printf("배열의 크기: \d", sizeof(name));
+	printf("id 배열의 크기: %d \n", sizeof(id));
 
 }
+
