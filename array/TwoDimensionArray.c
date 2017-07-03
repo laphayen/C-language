9e5396176926efcbcc1efb6db2688de154a0f19a
Baek Gi Chan <11618nathan@gmail.com>
2017-07-03 19:04:27 +0900

    Update TwoDimensionArray.c
    print two dimension array

--- array/TwoDimensionArray.c
+++ array/TwoDimensionArray.c
@@ -3,6 +3,19 @@
 
 void main()
 {
-	// 2Â÷¿ø ¹è¿­ ¼±¾ð
+	// 2차원 배열 선언
 	int Array[2][2] = {{1,6}, {1, 8}};
+
+	// 2차원 배열 출력
+	int row;
+	int col;
+
+	for (row = 0; row < 2; row++)
+	{
+		for (col = 0; col < 2; col++)
+		{
+			printf("Array[%d][%d] = [%d]", row, col, Array[row][col]);
+		}
+		puts("");
+	}
 }
