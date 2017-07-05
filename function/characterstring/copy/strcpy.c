74552d60251c2a06a3061011053c1415c55c507b
Baek Gi Chan <11618nathan@gmail.com>
2017-07-05 21:56:34 +0900

    Update strcpy.c

--- function/characterstring/copy/strcpy.c
+++ function/characterstring/copy/strcpy.c
@@ -5,6 +5,30 @@
 #include <string.h>
 
 void main()
+#include <stdio.h>
+
+// 문자열 함수 헤더 파일 선언
+#include <string.h>
+
+void main()
+{
+	// strcpy - 문자열 복사 함수
+	// destination에 전달된 값을 반환
+	char* strcpy(char *destination, const char *source);
+
+	char original[] = "11618nathan";
+
+	// 복사할 파일보다 배열의 크기 이상 지정 -> 오버플로우 방지
+	char copy[20];
+
+	// (복사할 위치, 위치);
+	strcpy(copy, original);
+
+	printf("%s \n", copy);
+}
+
+
+
 {
 	// strcpy - 문자열 복사 함수
 	// dest에 전달된 값을 반환
