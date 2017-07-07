c9c70d0df1e79b052422331322fa1fd4f62c23ef
Baek Gi Chan <11618nathan@gmail.com>
2017-07-07 23:19:07 +0900

    Update puts.c

--- function/output/puts.c
+++ function/output/puts.c
@@ -1,9 +1,14 @@
 
+// 헤더 파일 선언
 #include <stdio.h>
 
 void main()
 {
+	// puts - 문자열 출력 함수
+	// 함수 원형
+	int puts(const char * str);
 	char string[20] = "11618nathan";
 
+	// 출력 함수 - 개행
 	puts(string);
 }
