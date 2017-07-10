804c13876cb883866917883ab76f8b93e8ff8525
Baek Gi Chan <11618nathan@gmail.com>
2017-07-10 19:19:51 +0900

    Update malloc.c

--- pointer/malloc.c
+++ pointer/malloc.c
@@ -9,11 +9,13 @@ void main()
 	// malloc() - 동적 메모리 할당 - 동적으로 메모리를 할당하여 포인터를 변수에 저장
 	void * malloc(size_t size);
 
-	int * p;
-	x = (int *)malloc(sizeof(int));
+	int * p = (int*)malloc(sizeof(int));
+	*p = 1;
 
-	*p = 11618;
-	printf("%d \n", p);
+	printf("%d byte \n", _msize(p));
+	
+	printf("%d \n", *p);
 
+	// 할달 메모리 해제
 	free(p);
 }
