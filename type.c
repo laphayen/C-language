b939eb8a0f4f5ad2a69bbccf9ff144d4de6553f5
Baek Gi Chan <11618nathan@gmail.com>
2017-07-10 19:15:07 +0800

    Add files via upload

---
+++ type.c
@@ -0,0 +1,19 @@
+// 선언에서 변수 이름 생략 가능
+
+// 함수 종류 - void 매개변수, 리턴값 없음
+// 메개변수O, 리턴값O
+int fun1(int);
+
+// 메개변수O, 리턴값X
+int fun2(void);
+
+// 메개변수X, 리턴값O
+void fun3(char str);
+
+// 메개변수X, 리턴값X
+void fun4(void);
+
+void main()
+{
+
+}
