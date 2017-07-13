242a64214a472a831daf2330b2fe16686daa410f
Baek Gi Chan <11618nathan@gmail.com>
2017-07-13 23:32:05 +0900

    Update for-question.c

--- loop/for-question.c
+++ loop/for-question.c
@@ -1,14 +1,14 @@
 
 #include <stdio.h>
 
-// 1 - for ÇÕ°è
+// 1 - for 합계
 int main()
 {
 	int count = 1;
 	int sum = 0;
 	int input = 0;
 
-	printf("ÀÔ·Â: ");
+	printf("입력: ");
 	scanf("%d", &input);
 
 	for (count = 1; count <= input; count++)
@@ -16,5 +16,5 @@ int main()
 		sum += count;
 	}
 
-	printf("ÇÕ°è: %d \n", sum);
+	printf("합계: %d \n", sum);
 }
