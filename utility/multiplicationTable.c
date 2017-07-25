faf4af04a43ced2c3c360dfab497557c6d10624a
Baek Gi Chan <11618nathan@gmail.com>
2017-07-25 21:54:25 +0900

    Update multiplicationTable.c

--- utility/multiplicationTable.c
+++ utility/multiplicationTable.c
@@ -17,26 +17,4 @@ void main(void)
 		printf("%d * %d = %d \n", dan, num, dan*num);
 		num++;
 	}
-	
-	// while 중첩 - 전체 출력
-	while (dan<10)
-	{
-		num = 1;
-		while (num <10)
-		{
-			printf("%d * %d = %d \n", dan, num, dan*num);
-			num++;
-		}
-		dan++;
-	}
-
-	// for 중첩 - 전체 출력
-	for (dan = 2; dan < 10; dan++)
-	{
-		for (num = 0; num < 10; num++)
-		{
-			printf("%d * %d = %d \n", dan, num, dan*num);
-		}
-	}
-
 }
