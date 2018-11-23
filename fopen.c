c6576602cfe518784b2e856ae2785a56733d51c5
11618nathan <11618nathan@gmail.com>
2018-11-23 17:00:21 +0900

    Update

---
+++ fopen.c
@@ -0,0 +1,22 @@
+// 파일 읽기
+
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	char tep[30];
+
+	FILE *inp;
+
+	if ((inp = fopen("text.txt", "r")) == NULL)
+	{
+		printf("false");
+	}
+	
+	while (fgets(tep, 255, inp) != NULL)
+	{
+		printf("%s", tep);
+	}
+	fclose(inp);
+}
